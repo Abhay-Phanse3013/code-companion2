e641f4daf1cb36ba02906122fad8200979ab84e7
Abhay Phanse <52242762+Abhay-Phanse3013@users.noreply.github.com>
2026-04-28 15:12:06 +0530

    Remove deprecation notice banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Paper, Typography, useTheme } from "@mui/material"
+import { Paper, useTheme } from "@mui/material"
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from "./components/About";
@@ -11,37 +11,26 @@ function App() {
 
   return (
     <>
-    <Navbar />
-    <Paper sx={{
-      height: 'auto',
-      backgroundColor: theme.palette.background.default,
-      borderRadius: '0px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      paddingBlock: '12px'
-     }}>
-    <Typography variant="body2" maxWidth={"lg"}>Dear users, please be advised that this website is deprecated, and the project has concluded. We appreciate your support. You can find the code of this project here: <a href="https://github.com/akormous/code-companion" style={{ color: 'blue' }}>Frontend</a>, <a href="https://github.com/akormous/code-companion-server" style={{ color: 'blue' }}>Backend</a> </Typography><br />
-    </Paper>
-    <Paper
-     sx={{
-      minHeight: '92vh',
-      height: 'auto',
-      backgroundColor: theme.palette.background.default,
-      borderRadius: '0px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-     }}
-    >
-    <Routes>
-      <Route path="/about" element={<About />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/room/:id" element={<CodeRoom />} />
-      
-    </Routes>
-    
-        </Paper>        
+      <Navbar />
+      <Paper
+        sx={{
+          minHeight: '92vh',
+          height: 'auto',
+          backgroundColor: theme.palette.background.default,
+          borderRadius: '0px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/room/:id" element={<CodeRoom />} />
+
+        </Routes>
+
+      </Paper>
     </>
   )
 }
